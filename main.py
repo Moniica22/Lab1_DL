ae7849510e9bb40d010115875b55bfed03241c6e
tkarab <96498608+tkarab@users.noreply.github.com>
2026-05-06 16:27:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import scipy
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+import os
+import numpy as np
 
-models = ["FeedForward", "Lstm"]
+models = ["FeedForward", "Lstm", "Tcn"]
+models = ["Tcn"]
 
 def main():
 
@@ -10,16 +13,22 @@ def main():
 
     print("\n----Experimental Results for Each Model----\n")
     for model in models:
-        model_data = scipy.io.loadmat(f'{model}/predictions.mat')
-        model_preds = model_data['pred_var']
+        folder = f"{model}/predictions"
+        for i, file in enumerate(os.listdir(folder)):
+            model_data = scipy.io.loadmat(f'{model}/predictions/{file}')
+            model_preds = model_data['pred_var']
 
-        mae = mean_absolute_error(y, model_preds)
-        mse = mean_squared_error(y, model_preds)
-        print("-----")
-        print(f"Model Name: {model}")
-        print(f"MAE: {mae}")
-        print(f"MSE: {mse}")
-        print("-----")
+            # Check if needed to transpose
+            if model_preds.shape == (1, 200):
+                model_preds = np.transpose(model_preds)
+
+            mae = mean_absolute_error(y, model_preds)
+            mse = mean_squared_error(y, model_preds)
+            print("-----")
+            print(f"Model Name: {model} ({i+1})")
+            print(f"MAE: {mae}")
+            print(f"MSE: {mse}")
+            print("-----")
 
 
 
